3a83ed765594d6f4503482010bfba6a3ffef5efa
Kim Daekwon <propellerheaven@gmail.com>
2014-08-10 04:14:17 +0900

    Add diamond dockerfile

---
+++ diamond/run_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run -it\
+           -v /proc:/chroot/proc:ro \
+           -v /sys:/chroot/sys:ro \
+           -v /dev:/chroot/dev:ro \
+           -v /run:/chroot/run:ro \
+           nacyot/diamond

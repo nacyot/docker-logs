0872db7ac3142b0789d7b9e3db80b279bd25884d
toto <toto@smartstudy.co.kr>
2014-11-20 16:51:09 +0900

    Add service name log to sysmond config

--- newrelic-sysmond/busybox/files/run.sh
+++ newrelic-sysmond/busybox/files/run.sh
@@ -2,4 +2,5 @@
 
 nrsysmond-config --set license_key=$NEW_RELIC_LICENSE_KEY
 mv /etc/newrelic/nrsysmond.cfg /chroot/etc/newrelic/nrsysmond.cfg
+echo "labels=service:$SERVICE_NAME" >> /chroot/etc/newrelic/nrsysmond.cfg
 chroot /chroot /bin/nrsysmond -c /etc/newrelic/nrsysmond.cfg -n $NEW_RELIC_HOST_NAME -l /dev/stdout -f

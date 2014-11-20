82ac465edd5f63d35a36744cda62f3b07830c2f4
toto <toto@smartstudy.co.kr>
2014-11-20 16:58:51 +0900

    Add /dev directory to chroot

--- newrelic-sysmond/busybox/run_container_sample.sh
+++ newrelic-sysmond/busybox/run_container_sample.sh
@@ -5,5 +5,6 @@ docker run -d \
   -v /etc/resolv.conf:/chroot/etc/resolv.conf \
   -e NEW_RELIC_LICENSE_KEY=$NEW_RELIC_LICENSE_KEY \
   -e NEW_RELIC_HOST_NAME=`hostname` \
+  -e SERVICE_NAME=sample_service
   -h `hostname` \
   nacyot/newrelic-sysmond:busybox

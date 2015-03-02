5509da2bde923e5e36ebda6cb2544d23ac8ff135
nacyot <propellerheaven@gmail.com>
2015-03-02 18:02:36 +0900

    Route data to s3

--- td-agent/aggregator/setup_configs.sh
+++ td-agent/aggregator/setup_configs.sh
@@ -8,3 +8,7 @@ sed -i -e "s/::\(ES_TYPE_NAME\)::/${ES_TYPE_NAME}/g" /etc/td-agent/td-agent.conf
 sed -i -e "s/::\(ES_HOST\)::/${ES_HOST}/g" /etc/td-agent/td-agent.conf
 sed -i -e "s/::\(ES_PORT\)::/${ES_PORT}/g" /etc/td-agent/td-agent.conf
 sed -i -e "s/::\(FLUSH_INTERVAL\)::/${FLUSH_INTERVAL}/g" /etc/td-agent/td-agent.conf
+sed -i -e "s/::\(AWS_KEY_ID\)::/${AWS_KEY_ID}/g" /etc/td-agent/td-agent.conf
+sed -i -e "s/::\(AWS_SECRET_KEY\)::/${AWS_SECRET_KEY}/g" /etc/td-agent/td-agent.conf
+sed -i -e "s/::\(AWS_BUCKET\)::/${AWS_BUCKET}/g" /etc/td-agent/td-agent.conf
+sed -i -e "s/::\(AWS_REGION\)::/${AWS_REGION}/g" /etc/td-agent/td-agent.conf

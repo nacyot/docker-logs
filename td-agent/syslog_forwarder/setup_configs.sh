57e1f6de5b48c30cbf280f16661487e2f8465f7c
nacyot <propellerheaven@gmail.com>
2016-07-18 16:13:27 +0900

    Update td-agent config

--- td-agent/syslog_forwarder/setup_configs.sh
+++ td-agent/syslog_forwarder/setup_configs.sh
@@ -4,7 +4,7 @@ sed -i -e "s/::\(SYSLOG_TYPE\)::/${SYSLOG_TYPE}/g" /etc/td-agent/td-agent.conf
 sed -i -e "s/::\(SYSLOG_TAG\)::/${SYSLOG_TAG}/g" /etc/td-agent/td-agent.conf
 sed -i -e "s/::\(SYSLOG_BIND\)::/${SYSLOG_BIND}/g" /etc/td-agent/td-agent.conf
 sed -i -e "s/::\(SYSLOG_PORT\)::/${SYSLOG_PORT}/g" /etc/td-agent/td-agent.conf
-sed -i -e "s/::\(SYSLOG_FORMAT\)::/${SYSLOG_FORMAT}/g" /etc/td-agent/td-agent.conf
+sed -i -e "s|::\(SYSLOG_FORMAT\)::|${SYSLOG_FORMAT}|g" /etc/td-agent/td-agent.conf
 sed -i -e "s/::\(SYSLOG_TIME_FORMAT\)::/${SYSLOG_TIME_FORMAT}/g" /etc/td-agent/td-agent.conf
 sed -i -e "s/::\(FORWARD_HOST\)::/${FORWARD_HOST}/g" /etc/td-agent/td-agent.conf
 sed -i -e "s/::\(FORWARD_PORT\)::/${FORWARD_PORT}/g" /etc/td-agent/td-agent.conf
